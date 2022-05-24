c4bab7afc08e148fe68cf921fd2cdba489d426a5
vit-aborigen <vit.aborigen@gmail.com>
2022-05-24 12:21:53 +0300

    the very first commit

---
+++ theveryfirstapp/theveryfirstapp/theveryfirstappApp.swift
@@ -0,0 +1,17 @@
+//
+//  theveryfirstappApp.swift
+//  theveryfirstapp
+//
+//  Created by VITALI KAZLOU on 24.05.22.
+//
+
+import SwiftUI
+
+@main
+struct theveryfirstappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
