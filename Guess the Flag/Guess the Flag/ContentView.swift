b507ea192307e814995c32935f23e55b65048e9e
vit-aborigen <vit.aborigen@gmail.com>
2022-05-27 22:53:54 +0300

    part II

---
+++ Guess the Flag/Guess the Flag/ContentView.swift
@@ -0,0 +1,68 @@
+//
+//  ContentView.swift
+//  Guess the Flag
+//
+//  Created by VITALI KAZLOU on 26.05.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+    
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+    @State private var correctAnswer: Int = Int.random(in: 0...2)
+    
+    var body: some View {
+        ZStack {
+            Color.blue
+                .ignoresSafeArea()
+            
+            VStack (spacing: 30) {
+                VStack {
+                    Text("Tap the flag of")
+                    Text(countries[correctAnswer])
+                }
+                .foregroundColor(.white)
+                
+                ForEach(0..<3) { number in
+                    Button {
+                        flagTapped(number)
+                    } label: {
+                        Image(countries[number])
+                            .renderingMode(.original)
+                    }
+                }
+            }
+        }
+        .alert(scoreTitle, isPresented: $showingScore) {
+            Button ("Continue", action: askQuestion)
+        } message: {
+            Text("Your score is")
+        }
+    }
+        
+    func flagTapped (_ number: Int) {
+        if number == correctAnswer {
+            scoreTitle = "Correct"
+        } else {
+            scoreTitle = "Wrong"
+        }
+        
+        showingScore = true
+    }
+    
+    func askQuestion() {
+        countries.shuffled()
+        correctAnswer = Int.random(in: 0...2)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Guess the Flag/Guess the Flag/Guess_the_FlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guess_the_FlagApp.swift
+//  Guess the Flag
+//
+//  Created by VITALI KAZLOU on 26.05.22.
+//
+
+import SwiftUI
+
+@main
+struct Guess_the_FlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
