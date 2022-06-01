aa8cdf211b12a1d3b9ce7674319e9f1563796209
vit-aborigen <vit.aborigen@gmail.com>
2022-06-01 22:20:23 +0300

    Better Rest initial

---
+++ Better Rest/Better Rest/Better_RestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Better_RestApp.swift
+//  Better Rest
+//
+//  Created by VITALI KAZLOU on 1.06.22.
+//
+
+import SwiftUI
+
+@main
+struct Better_RestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Better Rest/Better Rest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Better Rest
+//
+//  Created by VITALI KAZLOU on 1.06.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
