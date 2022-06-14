4a2c9739ceb03e74335fa906e1adc290d81646f1
vit-aborigen <vit.aborigen@gmail.com>
2022-06-14 13:19:46 +0300

    Moonshot initial

---
+++ Moonshot/Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by VITALI KAZLOU on 14.06.22.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
