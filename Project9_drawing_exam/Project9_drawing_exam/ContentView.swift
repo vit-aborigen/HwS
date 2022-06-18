03a756f9e7fd960923faea71af9d79e6aaa67a03
vit-aborigen <vit.aborigen@gmail.com>
2022-06-18 23:23:01 +0300

    arrow part

--- Project9_drawing_exam/Project9_drawing_exam/ContentView.swift
+++ Project9_drawing_exam/Project9_drawing_exam/ContentView.swift
@@ -7,13 +7,51 @@
 
 import SwiftUI
 
+struct Arrow: Shape {
+    let arrowOffset = 40.0
+    var rotationAngle: Double
+    
+    var animatableData: Double {
+        get {
+            rotationAngle
+        }
+        set {
+            rotationAngle = newValue
+        }
+    }
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        path.move(to: CGPoint(x: 0, y: rect.midY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.midY))
+        path.addLine(to: CGPoint(x: rect.maxX - arrowOffset, y: rect.midY - arrowOffset))
+        path.addLine(to: CGPoint(x: rect.maxX - arrowOffset, y: rect.midY + arrowOffset))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.midY))
+        path.closeSubpath()
+        
+        let transform = CGAffineTransform(rotationAngle: rotationAngle)
+        return path.applying(transform)
+    }
+}
+
 struct ContentView: View {
+    @State private var rotationAngle = 0.0
     
     var body: some View {
-        VStack {
+            VStack {
+                Arrow(rotationAngle: rotationAngle)
+                    .stroke(.red, lineWidth: 10)
+                    .frame(width: 100, height: 100)
+                    .onTapGesture {
+                        withAnimation {
+                            rotationAngle = Double.random(in: -1.0...1.0)
+                        }
+                    }
         }
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
