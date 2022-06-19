8caa51147a86660b4c83030d45d0ca7bb4a2cfde
vit-aborigen <vit.aborigen@gmail.com>
2022-06-19 13:30:16 +0300

    Initial

---
+++ Habit tracker/Habit tracker/Habit_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Habit_trackerApp.swift
+//  Habit tracker
+//
+//  Created by VITALI KAZLOU on 19.06.22.
+//
+
+import SwiftUI
+
+@main
+struct Habit_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
