2c95c2e40bf5fdff07fabb96660c9ddf08091910
vit-aborigen <vit.aborigen@gmail.com>
2022-06-20 21:25:44 +0300

    Initial

---
+++ Cupcake corner/Cupcake corner/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Cupcake corner
+//
+//  Created by VITALI KAZLOU on 20.06.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+        Text("Test")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Cupcake corner/Cupcake corner/Cupcake_cornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cupcake_cornerApp.swift
+//  Cupcake corner
+//
+//  Created by VITALI KAZLOU on 20.06.22.
+//
+
+import SwiftUI
+
+@main
+struct Cupcake_cornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
