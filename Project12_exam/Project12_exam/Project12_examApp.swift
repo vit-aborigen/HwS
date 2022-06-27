3d2257862c34675ef61dd3f0a14bb6ca0efe412e
vit-aborigen <vit.aborigen@gmail.com>
2022-06-27 12:55:45 +0300

    prj 12 init

---
+++ Project12_exam/Project12_exam/Project12_examApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project12_examApp.swift
+//  Project12_exam
+//
+//  Created by VITALI KAZLOU on 27.06.22.
+//
+
+import SwiftUI
+
+@main
+struct Project12_examApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
