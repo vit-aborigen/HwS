c99d4f523bfc367d155833157215c9ff6d78bc2c
vit-aborigen <vit.aborigen@gmail.com>
2022-06-28 19:05:37 +0300

    FriendsFace init

---
+++ FriendsFace/FriendsFace/FriendsFaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendsFaceApp.swift
+//  FriendsFace
+//
+//  Created by VITALI KAZLOU on 28.06.22.
+//
+
+import SwiftUI
+
+@main
+struct FriendsFaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
