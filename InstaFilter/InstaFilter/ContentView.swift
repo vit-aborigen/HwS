ec787f5e89f40d3f46f3cf4167b1f4d907799789
vit-aborigen <vit.aborigen@gmail.com>
2022-07-02 21:20:35 +0300

    Filtering done

--- InstaFilter/InstaFilter/ContentView.swift
+++ InstaFilter/InstaFilter/ContentView.swift
@@ -5,12 +5,18 @@
 //  Created by VITALI KAZLOU on 1.07.22.
 //
 
+import CoreImage
+import CoreImage.CIFilterBuiltins
 import SwiftUI
-import PhotosUI
 
 struct ContentView: View {
     @State private var image: Image?
-    @State private var filterIntensity = 0.5
+    @State private var inputImage: UIImage?
+    @State private var filterIntensity: Float = 0.5
+    @State private var isImagePickerShown = false
+    
+    @State private var currentFilter = CIFilter.sepiaTone()
+    let context = CIContext()
     
     var body: some View {
         NavigationView {
@@ -28,14 +34,18 @@ struct ContentView: View {
                         .scaledToFit()
                 }
                 .onTapGesture {
-                    // 2do: load image
+                    isImagePickerShown = true
                 }
+                
                 
                 HStack {
                     Text("Intensity")
                     
                     Slider(value: $filterIntensity)
                         .padding(.vertical)
+                        .onChange(of: filterIntensity) { _ in
+                            applyProcessing()
+                        }
                 }
                 
                 HStack {
@@ -46,15 +56,45 @@ struct ContentView: View {
                     Spacer()
                     
                     Button("Save", action: save)
+                        .disabled(imageIsLoaded())
                 }
             }
             .padding([.horizontal, .bottom])
             .navigationTitle("InstaFilter")
+            .onChange(of: inputImage) { _ in loadImage()}
+            .sheet(isPresented: $isImagePickerShown) {
+                ImagePicker(image: $inputImage)
+            }
         }
+    }
+    
+    func loadImage() {
+        guard let inputImage = inputImage else { return }
+        let beginImage = CIImage(image: inputImage)
+        currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+        applyProcessing()
     }
     
     func save() {
         // 2do: save to the gallery
+    }
+    
+    func applyProcessing() {
+        currentFilter.intensity = filterIntensity
+        
+        guard let outputImage = currentFilter.outputImage else { return }
+        
+        if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
+            let uiImage = UIImage(cgImage: cgimg)
+            image = Image(uiImage: uiImage)
+        }
+    }
+    
+    func imageIsLoaded() -> Bool {
+        if let image = image {
+            return false
+        }
+        return true
     }
 }
 
