a73aeea2dd4e79cb507aa6e208470bc924bff257
vit-aborigen <vit.aborigen@gmail.com>
2022-07-06 13:26:05 +0300

    init

---
+++ BucketList/BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by VITALI KAZLOU on 5.07.22.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BucketList/BucketList/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  BucketList
+//
+//  Created by VITALI KAZLOU on 5.07.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    func getDocumentsDirectory() -> URL {
+        // find all possible documents directories for this user
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+
+        // just send back the first one, which ought to be the only one
+        return paths[0]
+    }
+    
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
