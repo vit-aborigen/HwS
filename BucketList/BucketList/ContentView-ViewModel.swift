5ca1da5a7831b7c5a876bc0f501d6faae3bca367
vit-aborigen <vit.aborigen@gmail.com>
2022-07-23 20:39:26 +0300

    part III done. right before MVVM

---
+++ BucketList/BucketList/ContentView-ViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ContentView-ViewModel.swift
+//  BucketList
+//
+//  Created by VITALI KAZLOU on 23.07.22.
+//
+
+import Foundation
--- BucketList/BucketList/LocationSheetView.swift
+++ BucketList/BucketList/LocationSheetView.swift
@@ -9,12 +9,19 @@ import Foundation
 import SwiftUI
 
 struct LocationSheetView: View {
+    enum LoadingState {
+        case loading, loaded, failed
+    }
+    
     @Environment(\.dismiss) var dismiss
     var location: Location
     var onSave: (Location) -> Void
     
     @State private var name: String
     @State private var description: String
+    
+    @State private var loadingState = LoadingState.loading
+    @State private var pages = Array<Page>()
     
     var body: some View {
         NavigationView {
@@ -23,6 +30,23 @@ struct LocationSheetView: View {
                     TextField("Place name", text: $name)
                     
                     TextField("Description", text: $description)
+                }
+                
+                Section("Nearby...") {
+                    switch loadingState {
+                    case .loading:
+                        Text("Loading")
+                    case .loaded:
+                        ForEach(pages, id: \.pageid) { page in
+                            Text(page.title)
+                                .font(.headline)
+                            + Text(": ")
+                            + Text(page.description)
+                                .italic()
+                        }
+                    case .failed:
+                        Text("Failed to load. Please, try again later")
+                    }
                 }
             }
             .navigationTitle("Details")
@@ -47,6 +71,9 @@ struct LocationSheetView: View {
                     }
                 }
             }
+            .task {
+                await fetchNearbyPlaces()
+            }
         }
     }
     
@@ -56,6 +83,26 @@ struct LocationSheetView: View {
         
         self._name = State(initialValue: location.name)
         self._description = State(initialValue: location.description)
+    }
+    
+    func fetchNearbyPlaces() async {
+        let urlString = "https://en.wikipedia.org/w/api.php?ggscoord=\(location.coordinate.latitude)%7C\(location.coordinate.longitude)&action=query&prop=coordinates%7Cpageimages%7Cpageterms&colimit=50&piprop=thumbnail&pithumbsize=500&pilimit=50&wbptterms=description&generator=geosearch&ggsradius=10000&ggslimit=50&format=json"
+        
+        guard let url = URL(string: urlString) else {
+            print("Bad URL: ", urlString)
+            return
+        }
+        
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            
+            let items = try JSONDecoder().decode(Result.self, from: data)
+            
+            pages = items.query.pages.values.sorted()
+            loadingState = .loaded
+        } catch {
+            loadingState = .failed
+        }
     }
 }
 
