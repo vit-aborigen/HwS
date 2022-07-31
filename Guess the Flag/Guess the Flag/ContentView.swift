24e10714b62cf3312ed7cda5f6c9c293efb7fcd6
vit-aborigen <vit.aborigen@gmail.com>
2022-07-31 17:31:11 +0300

    accessibility added

--- Guess the Flag/Guess the Flag/ContentView.swift
+++ Guess the Flag/Guess the Flag/ContentView.swift
@@ -54,8 +54,7 @@ struct FlagImage: View {
         Image(countryName)
             .renderingMode(.original)
             .clipShape(Capsule(style: .continuous))
-            .shadow(radius: 5)
-    }
+            .shadow(radius: 5)    }
 }
 
 struct ContentView: View {
@@ -71,6 +70,20 @@ struct ContentView: View {
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
     @State private var correctAnswer: Int = Int.random(in: 0...2)
     @State private var userFlag = -1
+    
+    let labels = [
+        "Estonia": "Flag with three horizontal stripes of equal size. Top stripe blue, middle stripe black, bottom stripe white",
+        "France": "Flag with three vertical stripes of equal size. Left stripe blue, middle stripe white, right stripe red",
+        "Germany": "Flag with three horizontal stripes of equal size. Top stripe black, middle stripe red, bottom stripe gold",
+        "Ireland": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe orange",
+        "Italy": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe red",
+        "Nigeria": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe green",
+        "Poland": "Flag with two horizontal stripes of equal size. Top stripe white, bottom stripe red",
+        "Russia": "Flag with three horizontal stripes of equal size. Top stripe white, middle stripe blue, bottom stripe red",
+        "Spain": "Flag with three horizontal stripes. Top thin stripe red, middle thick stripe gold with a crest on the left, bottom thin stripe red",
+        "UK": "Flag with overlapping red and white crosses, both straight and diagonally, on a blue background",
+        "US": "Flag with red and white stripes of equal size, with white stars on a blue background in the top-left corner"
+    ]
     
     var body: some View {
         ZStack {
@@ -100,6 +113,7 @@ struct ContentView: View {
                             flagTapped(number)
                         } label: {
                             FlagImage(countryName: countries[number])
+                                .accessibilityLabel(labels[countries[number]] ?? "UK")
                                 .rotation3DEffect(Angle(degrees: userFlag == number ? 360.0 : 0), axis: (x: 0, y: 1, z: 0))
                                 .opacity(userFlag == -1 || userFlag == number ? 1 : 0.25)
                                 .animation(.interpolatingSpring(stiffness: 25, damping: 40), value: userFlag)
