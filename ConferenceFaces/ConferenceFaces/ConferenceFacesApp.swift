dd0ed7e7ff8a8b5a287b6c610abc419807716383
vit-aborigen <vit.aborigen@gmail.com>
2022-08-09 13:23:06 +0300

    init

---
+++ ConferenceFaces/ConferenceFaces/ConferenceFacesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConferenceFacesApp.swift
+//  ConferenceFaces
+//
+//  Created by VITALI KAZLOU on 9.08.22.
+//
+
+import SwiftUI
+
+@main
+struct ConferenceFacesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
