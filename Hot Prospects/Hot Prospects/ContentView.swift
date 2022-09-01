9ef25c2365cf2c27adfa7b7f83ad0ae7d7afde1e
vit-aborigen <vit.aborigen@gmail.com>
2022-09-01 21:49:09 +0300

    Init

---
+++ Hot Prospects/Hot Prospects/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Hot Prospects
+//
+//  Created by VITALI KAZLOU on 1.09.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Hot Prospects/Hot Prospects/Hot_ProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hot_ProspectsApp.swift
+//  Hot Prospects
+//
+//  Created by VITALI KAZLOU on 1.09.22.
+//
+
+import SwiftUI
+
+@main
+struct Hot_ProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
