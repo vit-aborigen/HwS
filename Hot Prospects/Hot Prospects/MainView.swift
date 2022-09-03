7c2b3f34ac3b5c419ff5de8ce850397e28010644
vit-aborigen <vit.aborigen@gmail.com>
2022-09-03 20:51:09 +0300

    day 80 done

--- Hot Prospects/Hot Prospects/MainView.swift
+++ Hot Prospects/Hot Prospects/MainView.swift
@@ -7,35 +7,23 @@
 
 import SwiftUI
 
-@MainActor class User: ObservableObject {
-    @Published var userName = "test user"
-}
-
 struct MainView: View {
-    @StateObject var user = User()
+    @State private var selectedTab: Int = 2
     
     var body: some View {
-        VStack {
-            EditView()
-            DisplayView()
+        TabView(selection: $selectedTab) {
+            Text("1")
+                .tabItem {
+                    Label("One", systemImage: "1.circle")
+                }
+                .tag(1)
+            
+            Text("2")
+                .tabItem {
+                    Label("Two", systemImage: "2.circle")
+                }
+                .tag(2)
         }
-        .environmentObject(user)
-    }
-}
-
-struct EditView: View {
-    @EnvironmentObject var user: User
-    
-    var body: some View {
-        TextField("Name: ", text: $user.userName)
-    }
-}
-
-struct DisplayView: View {
-    @EnvironmentObject var user: User
-    
-    var body: some View {
-        Text(user.userName)
     }
 }
 
