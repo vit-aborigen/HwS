a1f66f23be3665034b78f8c3b14a89ff9ba43d5b
vit-aborigen <vit.aborigen@gmail.com>
2022-09-06 22:38:39 +0300

    filtered results

--- Hot Prospects/Hot Prospects/UI/ProspectsView.swift
+++ Hot Prospects/Hot Prospects/UI/ProspectsView.swift
@@ -11,13 +11,23 @@ struct ProspectsView: View {
     enum FilterType {
         case none, contacted, uncontacted
     }
-    
+
     @EnvironmentObject var prospects: Prospects
     let filter: FilterType
-    
+
     var body: some View {
         NavigationView {
-            Text("People: \(prospects.people.count)")
+            List {
+                ForEach(filteredProspects) { prospect in
+                    VStack(alignment: .leading) {
+                        Text(prospect.name)
+                            .font(.headline)
+                        
+                        Text(prospect.email)
+                            .foregroundColor(.secondary)
+                    }
+                }
+            }
                 .navigationTitle(title)
                 .toolbar {
                     Button {
@@ -29,11 +39,9 @@ struct ProspectsView: View {
                         Label("Scan", systemImage: "qrcode.viewfinder")
                     }
                 }
-            
-
         }
     }
-    
+
     var title: String {
         switch filter {
         case .none:
@@ -42,6 +50,17 @@ struct ProspectsView: View {
             return "Contacted People"
         case .uncontacted:
             return "Uncontacted People"
+        }
+    }
+    
+    var filteredProspects: [Prospect] {
+        switch filter {
+        case .none:
+            return prospects.people
+        case .contacted:
+            return prospects.people.filter { $0.isContacted }
+        case .uncontacted:
+            return prospects.people.filter { !$0.isContacted }
         }
     }
 }
