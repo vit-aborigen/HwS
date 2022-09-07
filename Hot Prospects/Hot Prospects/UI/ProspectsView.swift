b3654adcd419d7b8b609b30547d9b261a06e72d4
vit-aborigen <vit.aborigen@gmail.com>
2022-09-07 22:44:53 +0300

    qr scanning

--- Hot Prospects/Hot Prospects/UI/ProspectsView.swift
+++ Hot Prospects/Hot Prospects/UI/ProspectsView.swift
@@ -5,6 +5,7 @@
 //  Created by VITALI KAZLOU on 6.09.22.
 //
 
+import CodeScanner
 import SwiftUI
 
 struct ProspectsView: View {
@@ -13,6 +14,8 @@ struct ProspectsView: View {
     }
 
     @EnvironmentObject var prospects: Prospects
+    @State private var isShowingScanner = false
+    
     let filter: FilterType
 
     var body: some View {
@@ -31,13 +34,13 @@ struct ProspectsView: View {
                 .navigationTitle(title)
                 .toolbar {
                     Button {
-                        let prospect = Prospect()
-                        prospect.name = "Test name"
-                        prospect.email = "test@gmail.com"
-                        prospects.people.append(prospect)
+                        isShowingScanner = true
                     } label: {
                         Label("Scan", systemImage: "qrcode.viewfinder")
                     }
+                }
+                .sheet(isPresented: $isShowingScanner) {
+                    CodeScannerView(codeTypes: [.qr], simulatedData: "Test person for a simulator\ntest@gmail.com", completion: handleScan)
                 }
         }
     }
@@ -61,6 +64,23 @@ struct ProspectsView: View {
             return prospects.people.filter { $0.isContacted }
         case .uncontacted:
             return prospects.people.filter { !$0.isContacted }
+        }
+    }
+    
+    func handleScan(result: Result<ScanResult, ScanError>) {
+        isShowingScanner = false
+        
+        switch result {
+        case .success(let result):
+            let details = result.string.components(separatedBy: "\n")
+            guard details.count == 2 else { return }
+            
+            let person = Prospect()
+            person.name = details[0]
+            person.email = details[1]
+            prospects.people.append(person)
+        case .failure(let error):
+            print(error.localizedDescription)
         }
     }
 }
