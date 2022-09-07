7454329d5426cc6655f21734c2c8c19ec1ded18d
vit-aborigen <vit.aborigen@gmail.com>
2022-09-07 20:37:04 +0300

    qrCode generation

--- Hot Prospects/Hot Prospects/UI/MeView.swift
+++ Hot Prospects/Hot Prospects/UI/MeView.swift
@@ -5,11 +5,47 @@
 //  Created by VITALI KAZLOU on 6.09.22.
 //
 
+import CoreImage
+import CoreImage.CIFilterBuiltins
 import SwiftUI
 
 struct MeView: View {
+    @State private var name = "Please enter your name"
+    @State private var email = "test@gmail.com"
+    
+    let context = CIContext()
+    let filter = CIFilter.qrCodeGenerator()
+    
     var body: some View {
-        Text("About")
+        NavigationView {
+            Form {
+                TextField("Name", text: $name)
+                    .font(.title)
+                    .textContentType(.name)
+                
+                TextField("Email", text: $email)
+                    .font(.title)
+                    .textContentType(.emailAddress)
+                
+                Image(uiImage: generateQRCode(from: "\(name)\n\(email)"))
+                    .resizable()
+                    .interpolation(.none)
+                    .scaledToFit()
+                    .frame(width: 200, height: 200)
+            }
+            .navigationTitle("Your code")
+        }
+    }
+    
+    func generateQRCode(from str: String) -> UIImage {
+        filter.message = Data(str.utf8)
+        if let outputImage = filter.outputImage {
+            if let cgImage = context.createCGImage(outputImage, from: outputImage.extent) {
+                    return UIImage(cgImage: cgImage)
+            }
+        }
+        
+        return UIImage(systemName: "xmark.circle") ?? UIImage()
     }
 }
 
