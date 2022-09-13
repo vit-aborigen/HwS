d7809a8fb63f5df30788f56beba75585a1a3acb5
vit-aborigen <vit.aborigen@gmail.com>
2022-09-13 15:37:20 +0300

    scenePhase and timer

--- Flashzilla/Flashzilla/MainView.swift
+++ Flashzilla/Flashzilla/MainView.swift
@@ -9,48 +9,28 @@ import CoreHaptics
 import SwiftUI
 
 struct MainView: View {
-    @State private var engine: CHHapticEngine?
+    @State private var text = ""
+    @Environment(\.scenePhase) var scenePhase
+    
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     
     var body: some View {
-        Text("")
-            .onAppear(perform: prepareHaptics)
-            .onTapGesture(perform: complexSuccess)
-    }
-    
-    func prepareHaptics() {
-        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
-        
-        do {
-            engine = try? CHHapticEngine()
-            try? engine?.start()
-        }
-    }
-    
-    func complexSuccess() {
-        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
-        var events = [CHHapticEvent]()
-
-        for i in stride(from: 0, to: 1, by: 0.1) {
-            let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: Float(i))
-            let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: Float(i))
-            let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: i)
-            events.append(event)
-        }
-
-        for i in stride(from: 0, to: 1, by: 0.1) {
-            let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: Float(1 - i))
-            let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: Float(1 - i))
-            let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: 1 + i)
-            events.append(event)
-        }
-
-        do {
-            let pattern = try CHHapticPattern(events: events, parameters: [])
-            let player = try engine?.makePlayer(with: pattern)
-            try player?.start(atTime: 0)
-        } catch {
-            print("Failed to play pattern: \(error.localizedDescription).")
-        }
+        Text(text)
+            .onReceive(timer) { time in
+                text = time.description
+            }
+            .onChange(of: scenePhase) { newScenePhase in
+                switch newScenePhase {
+                case .active:
+                    print("Active")
+                case .background:
+                    print("Background")
+                case .inactive:
+                    print("Inactive")
+                @unknown default:
+                    print("Unknown")
+                }
+            }
     }
 }
 
