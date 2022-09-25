c54df8e9d7f6c9ef29e20c250aa6d3bd8be5c0af
vit-aborigen <vit.aborigen@gmail.com>
2022-09-25 21:15:59 +0300

    Project 18. init

---
+++ Layout and Geometry/Layout and Geometry/Layout_and_GeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Layout_and_GeometryApp.swift
+//  Layout and Geometry
+//
+//  Created by VITALI KAZLOU on 25.09.22.
+//
+
+import SwiftUI
+
+@main
+struct Layout_and_GeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
