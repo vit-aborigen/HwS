f0d518499ef5f2b53b94d987b940b0ec9bbdf899
vit-aborigen <vit.aborigen@gmail.com>
2022-10-04 17:17:57 +0300

    dynamic type size

--- SnowSeeker/SnowSeeker/ResortView.swift
+++ SnowSeeker/SnowSeeker/ResortView.swift
@@ -10,10 +10,12 @@ import SwiftUI
 struct ResortView: View {
     let resort: Resort
     
+    @Environment(\.horizontalSizeClass) var sizeClass
+    @Environment(\.dynamicTypeSize) var typeSize
+    
     var body: some View {
         ScrollView {
             VStack(alignment: .leading, spacing: 0) {
-                
                 ZStack(alignment: .bottomTrailing) {
                     Image(resort.id)
                         .resizable()
@@ -26,11 +28,21 @@ struct ResortView: View {
             }
             
             HStack {
-                ResortDetailsView(resort: resort)
-                SkiDetailsView(resort: resort)
+                if sizeClass == .compact && typeSize > .large {
+                    VStack(spacing: 10) {
+                        ResortDetailsView(resort: resort)
+                    }
+                    VStack(spacing: 10) {
+                        SkiDetailsView(resort: resort)
+                    }
+                } else {
+                    ResortDetailsView(resort: resort)
+                    SkiDetailsView(resort: resort)
+                }
             }
             .padding(.vertical)
             .background(.primary.opacity(0.1))
+            .dynamicTypeSize(...DynamicTypeSize.xxxLarge)
             
             Group {
                 Text(resort.description)
