e095c96033638bb517e571a1bc1b9a1aae3473b8
vit-aborigen <vit.aborigen@gmail.com>
2022-10-07 21:59:45 +0300

    Saving / loading data

--- SnowSeeker/SnowSeeker/Favorites.swift
+++ SnowSeeker/SnowSeeker/Favorites.swift
@@ -7,12 +7,27 @@
 
 import Foundation
 
-class Favorites: ObservableObject {
+class Favorites: Codable, ObservableObject {
     private var resorts: Set<String>
-    private let saveKey = "Favorites"
+    private var savePath = FileManager.documentsDirectory.appendingPathComponent("snowseeker_favorites.json")
     
     init() {
-        resorts = []
+        do {
+            let data = try Data(contentsOf: savePath)
+            resorts = try JSONDecoder().decode(Set<String>.self, from: data)
+        } catch {
+            print("Failed to load data: ", error.localizedDescription)
+            resorts = []
+        }
+    }
+    
+    func saveData() {
+        do {
+            let data = try JSONEncoder().encode(resorts)
+            try data.write(to: savePath, options: [.atomic, .completeFileProtection])
+        } catch {
+            print("Failed to encode data: ", error.localizedDescription)
+        }
     }
     
     func contains(_ resort: Resort) -> Bool {
@@ -22,14 +37,12 @@ class Favorites: ObservableObject {
     func add(_ resort: Resort) {
         objectWillChange.send()
         resorts.insert(resort.id)
-        save()
+        saveData()
     }
     
     func delete(_ resort: Resort) {
         objectWillChange.send()
         resorts.remove(resort.id)
-        save()
+        saveData()
     }
-    
-    func save() {}
 }
