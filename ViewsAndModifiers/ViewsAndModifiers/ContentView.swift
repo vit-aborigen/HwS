95c310f5628dd6ff160550522c96ee5364453252
vit-aborigen <vit.aborigen@gmail.com>
2023-01-16 23:22:32 +0300

    Dim screen prototype for biorhythms

--- ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -8,29 +8,66 @@
 import SwiftUI
 
 struct ContentView: View {
-    let image = UIImage(systemName: "questionmark.circle")!
-
+    var isUserNewToBiorhythms = true
+    
     var body: some View {
-        Image(uiImage: image)
-            .onTapGesture {
-                let savePath = FileManager.documentsDirectory
-                
-                if let data = image.jpegData(compressionQuality: 0.8) {
-                    let filename = savePath.appendingPathComponent("MyConferences\newImage.jpg")
-                    do {
-                        try data.write(to: filename)
-                    } catch {
-                        print("Failed to save image: \(error.localizedDescription)")
-                    }
-                }
+        ZStack {
+            CurrentScreen()
+
+            if isUserNewToBiorhythms {
+                DimScreen(placeHintAt: CGSize(width: 50, height: 100), hintSize: CGSize(width: 100, height: 300))
             }
+        }
     }
 }
 
+struct CurrentScreen: View {
+    var body: some View {
+        NavigationView {
+            VStack {
+                ZStack {
+                    Color.red
+                    Text("Top")
+                }
 
-extension FileManager {
-    static var documentsDirectory: URL {
-        let path = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
-        return path[0]
+                ZStack {
+                    Color.green
+                    Text("Middle")
+                }
+
+                ZStack {
+                    Color.blue
+                    Text("Bottom")
+                }
+            }
+        }
+    }
+}
+
+struct DimScreen: View {
+    let placeHintAt: CGSize
+    let hintSize: CGSize
+
+    init(placeHintAt: CGSize, hintSize: CGSize) {
+        self.placeHintAt = placeHintAt
+        self.hintSize = hintSize
+    }
+
+    var body: some View {
+        ZStack {
+            Color.black.opacity(0.8)
+
+            Rectangle()
+                .frame(width: hintSize.width, height: hintSize.height)
+                .offset(placeHintAt)
+                .blendMode(.destinationOut)
+        }
+        .compositingGroup()
+    }
+}
+
+struct ContentView_Preview: PreviewProvider {
+    static var previews: some View {
+        return ContentView()
     }
 }
